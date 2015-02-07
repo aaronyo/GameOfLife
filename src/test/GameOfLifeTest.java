36ca86e3beeac82cde15fd5903208b33bde26e1e
Ricardo Schaefli <ricardo.schaefli@gmail.com>
2015-02-07 15:25:45 -0500

    Another test case...I need a better way to write these

--- src/test/GameOfLifeTest.java
+++ src/test/GameOfLifeTest.java
@@ -146,6 +146,77 @@ public class GameOfLifeTest {
 		assertBoardsEqual(board, game.getBoard());
 	}
 	
+	@Test
+	public void stableCase2()
+	{
+		Boolean[][] board = new Boolean[5][5];
+		board[0][0] = false;
+		board[0][1] = false;
+		board[0][2] = false;
+		board[0][3] = false;
+		board[0][4] = false;
+		
+		board[1][0] = false;
+		board[1][1] = false;
+		board[1][2] = false;
+		board[1][3] = false;
+		board[1][4] = false;
+		
+		board[2][0] = true;
+		board[2][1] = true;
+		board[2][2] = true;
+		board[2][3] = true;
+		board[2][4] = false;
+		
+		board[3][0] = false;
+		board[3][1] = false;
+		board[3][2] = false;
+		board[3][3] = false;
+		board[3][4] = false;
+		
+		board[4][0] = false;
+		board[4][1] = false;
+		board[4][2] = false;
+		board[4][3] = false;
+		board[4][4] = false;
+		
+		GameOfLife game = new GameOfLife(1, board);
+		game.run();
+		
+		Boolean[][] expectedBoard = new Boolean[5][5];
+		expectedBoard[0][0] = false;
+		expectedBoard[0][1] = false;
+		expectedBoard[0][2] = false;
+		expectedBoard[0][3] = false;
+		expectedBoard[0][4] = false;
+		
+		expectedBoard[1][0] = false;
+		expectedBoard[1][1] = true;
+		expectedBoard[1][2] = true;
+		expectedBoard[1][3] = false;
+		expectedBoard[1][4] = false;
+		
+		expectedBoard[2][0] = false;
+		expectedBoard[2][1] = true;
+		expectedBoard[2][2] = true;
+		expectedBoard[2][3] = false;
+		expectedBoard[2][4] = false;
+		
+		expectedBoard[3][0] = false;
+		expectedBoard[3][1] = true;
+		expectedBoard[3][2] = true;
+		expectedBoard[3][3] = false;
+		expectedBoard[3][4] = false;
+		
+		expectedBoard[4][0] = false;
+		expectedBoard[4][1] = false;
+		expectedBoard[4][2] = false;
+		expectedBoard[4][3] = false;
+		expectedBoard[4][4] = false;
+		
+		assertBoardsEqual(expectedBoard, game.getBoard());
+	}
+	
 	private void assertBoardsEqual(Boolean[][] expected, Boolean[][] actual)
 	{
 		for(int i=0;i<actual.length;i++)
